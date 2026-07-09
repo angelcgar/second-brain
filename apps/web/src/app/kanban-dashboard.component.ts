5e051fcd1a18b3cd9fbab1850a43082ebae4e8c9
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2026-07-09 17:53:31 -0600

    feat(web): agregar el board de Goals

--- apps/web/src/app/kanban-dashboard.component.ts
+++ apps/web/src/app/kanban-dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KanbanBoardTaskComponent } from './kanban-board/kanban-board-task.component';
 import { KanbanBoardProjectsComponent } from './kanban-board/kanban-board-projects.component';
+import { KanbanBoardGoalsComponent } from './kanban-board/kanban-board-goals.component';
 import { KanbanBoardAreasComponent } from './kanban-board/kanban-board-areas.component';
 
 @Component({
@@ -11,12 +12,14 @@ import { KanbanBoardAreasComponent } from './kanban-board/kanban-board-areas.com
     CommonModule,
     KanbanBoardTaskComponent,
     KanbanBoardProjectsComponent,
+    KanbanBoardGoalsComponent,
     KanbanBoardAreasComponent,
   ],
   template: `
     <div class="min-h-screen w-screen bg-[#191919] flex flex-col overflow-y-auto overflow-x-hidden gap-8 pb-12">
       <app-kanban-board-task></app-kanban-board-task>
       <app-kanban-board-projects></app-kanban-board-projects>
+      <app-kanban-board-goals></app-kanban-board-goals>
       <app-kanban-board-areas></app-kanban-board-areas>
     </div>
   `,
